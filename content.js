9fe299de53233019ed7a458060bed811fad68f36
unscodst <spikester@sbcglobal.net>
2015-06-18 04:17:58 -0400

    Class Name
    Can now display the class but need to clean up the string.

--- content.js
+++ content.js
@@ -1,6 +1,16 @@
-var className = $('th').text();
-
-
+/*
+var className = $('th.ddlabel:nth-child(1)').text();
 alert('Class: ' + className);
-
 console.log(className);
+*/
+
+$("tr:nth-child(1)").each(function (index) {
+    if (index == 2) alert('Class : ' + $(this).html());
+});
+
+
+/*$(document).on("click", ".btn", function (e) {    
+    $('.content > table td').each(function(index){
+        if(index == 4) alert($(this).text());
+    });
+});*/
