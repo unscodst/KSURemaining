cc3bd81025e11c8aebc0657bc301b7e177fc38a6
unscodst <spikester@sbcglobal.net>
2015-06-18 06:19:31 -0400

    KSU Keys Only
    changed "matches" in manifest to inject only on KSU Keys page.

--- content.js
+++ content.js
@@ -1,13 +1,14 @@
 $("tr:nth-child(1)").each(function (index) {
     var str = $(this).html();
     var clean = str.replace(/(<th[^>]*>|<\/th>|<br>)/g, "");
-    
+
     if (index == 2) alert('Class : ' + clean);
 });
 
 $("td:nth-child(n)").each(function (index) {
     var str = $(this).html();
     var clean = str.replace(/(<th[^>]*>|<\/th>|<br>)/g, "");
-    
+
     if (index == 10) alert('Seats : ' + str);
 });
+
