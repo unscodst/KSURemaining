b408f7033c1f7ec716eded14ab83032995457f45
unscodst <spikester@sbcglobal.net>
2015-06-17 22:33:17 -0400

    Browser Tab
    Currently working and opens tab to CRN. Only opens in browser, won't
    open from extension.

--- url.js
+++ url.js
@@ -1,8 +1,10 @@
 function OpenPage() {
     "use strict";
     var crn = document.getElementById("c1").value,
+
         ksu = "https://keys.kent.edu/ePROD/bwckschd.p_disp_detail_sched?term_in=201580&crn_in=",
         url = ksu + crn;
     window.open(url, '_blank');
+
 
 }
