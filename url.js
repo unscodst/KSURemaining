e73b402616da7105670722c79d389bf868d200c0
unscodst <spikester@sbcglobal.net>
2015-06-18 00:48:14 -0400

    Tabs and Scripts
    When you click the submit button a tab will open up to KSU Keys. After
    following a bunch of example online I finally found out that the script
    need to be placed at the bottom of the HTML page <before /body>. I'm not
    sure why this fixed the problem.

--- url.js
+++ url.js
@@ -1,10 +1,9 @@
-function OpenPage() {
+function showIndex() {
     "use strict";
     var crn = document.getElementById("c1").value,
-
         ksu = "https://keys.kent.edu/ePROD/bwckschd.p_disp_detail_sched?term_in=201580&crn_in=",
-        url = ksu + crn;
-    window.open(url, '_blank');
-
-
+        index_url = ksu + crn;
+    chrome.tabs.create({url: index_url});
 }
+
+document.getElementById('submit').addEventListener("click", showIndex);
