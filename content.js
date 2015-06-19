e075aa49a4f9b1111f78855bed56e6d89bb65834
unscodst <spikester@sbcglobal.net>
2015-06-18 20:47:20 -0400

    Minor rearranging and fixes
    Created working <tutorial> button for popup. Rearranged manifest based
    on load structure discovered while researching, idk if it actually
    changed anything/made it work better. Changed <alert> to <console.log>
    after I broke the alerts and accidently told chrome to stop loading
    them, this works better anyways.
    
    TODO: Get results to show in popup. Attempting this with
    <chrome.runtime.sendMessage & onMessage> nothing is working yet.

--- content.js
+++ content.js
@@ -2,13 +2,18 @@ $("tr:nth-child(1)").each(function (index) {
     var str = $(this).html();
     var clean = str.replace(/(<th[^>]*>|<\/th>|<br>)/g, "");
 
-    if (index == 2) alert('Class : ' + clean);
+    if (index == 2) console.log('Class : ' + clean);
 });
 
 $("td:nth-child(n)").each(function (index) {
     var str = $(this).html();
-    var clean = str.replace(/(<th[^>]*>|<\/th>|<br>)/g, "");
+    if (index == 10) console.log('Seats : ' + str);
 
-    if (index == 10) alert('Seats : ' + str);
 });
 
+
+
+chrome.runtime.sendMessage({greeting: "hello"},
+    function (response) {
+    console.log(response.farewell);
+});
--- url.js
+++ url.js
@@ -5,6 +5,23 @@ function showIndex() {
         index_url = ksu + crn;
     chrome.tabs.create({url: index_url});
 }
-
 document.getElementById('submit').addEventListener("click", showIndex);
 
+function tutorial() {
+    "use strict";
+    var tut = "https://youtu.be/b3utCM0zH5I";
+    chrome.tabs.create({url: tut});
+}
+document.getElementById('tut').addEventListener("click", tutorial);
+
+
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        console.log(sender.tab ?
+                    "from a content script:" + sender.tab.url :
+                    "from the extension");
+        if (request.greeting == "hello")
+            sendResponse({farewell: "goodbye"});
+    }
+);
